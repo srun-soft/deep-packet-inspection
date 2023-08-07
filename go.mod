63cb9d213274f2eb9a93189007c35a39aaccca16
原桐 <yt.vertigo0927@gmail.com>
2023-08-07 17:38:59 +0800

    删除输出网卡方法

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/srun-soft/dpi-analysis-toolkit
 
 go 1.19
 
-require github.com/google/gopacket v1.1.19 // indirect
+require (
+	github.com/google/gopacket v1.1.19 // indirect
+	golang.org/x/net v0.14.0 // indirect
+)
--- internal/packet_capture/packet_capture.go
+++ internal/packet_capture/packet_capture.go
@@ -3,7 +3,6 @@ package packet_capture
 import (
 	"bufio"
 	"flag"
-	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/examples/util"
 	"github.com/google/gopacket/layers"
@@ -115,23 +114,5 @@ func init() {
 			assembler.FlushOlderThan(time.Now().Add(time.Minute * -2))
 		}
 
-	}
-}
-
-func FindAllDev() {
-	ifs, err := pcap.FindAllDevs()
-	if err != nil {
-		_ = fmt.Errorf("find all devs err:%v", err)
-	}
-	for _, item := range ifs {
-		fmt.Printf("Name:%s\n", item.Name)
-		fmt.Printf("Description:%s\n", item.Description)
-		fmt.Printf("Flags:%d\n", item.Flags)
-		for _, addr := range item.Addresses {
-			fmt.Printf("IP:%s\n", addr.IP)
-			fmt.Printf("NetMask:%s\n", addr.Netmask)
-			fmt.Printf("Broadaddr:%s\n", addr.Broadaddr)
-			fmt.Printf("P2P:%s\n", addr.P2P)
-		}
 	}
 }
