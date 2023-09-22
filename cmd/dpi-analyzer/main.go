001f0bde9da579468d6f1339963bd3c5f419e25e
原桐 <yt.vertigo0927@gmail.com>
2023-09-22 16:49:46 +0800

    mongodb初始化

--- cmd/dpi-analyzer/main.go
+++ cmd/dpi-analyzer/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	_ "github.com/srun-soft/dpi-analysis-toolkit/configs"
-	_ "github.com/srun-soft/dpi-analysis-toolkit/internal/packet_capture"
+	_ "github.com/srun-soft/dpi-analysis-toolkit/internal/database"
+	//_ "github.com/srun-soft/dpi-analysis-toolkit/internal/packet_capture"
 )
 
 func main() {
+
 }
--- internal/database/mongodb.go
+++ internal/database/mongodb.go
@@ -1,1 +1,36 @@
 package database
+
+import (
+	"context"
+	"github.com/srun-soft/dpi-analysis-toolkit/configs"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+var (
+	MongoDB *mongo.Database
+)
+
+func init() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	// 设置客户端连接配置 TODO 更换配置地址与端口
+	opts := options.Client().ApplyURI("mongodb://localhost:27017/dpi")
+	client, err := mongo.Connect(ctx, opts)
+	if err != nil {
+		configs.Log.Fatal("Failed to Connect MongoDB!", err)
+	}
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			configs.Log.Fatal("Failed to Disconnect MongoDB!", err)
+		}
+	}()
+
+	// check connect
+	if err = client.Ping(ctx, nil); err != nil {
+		configs.Log.Fatal("Failed to check connect MongoDB!", err)
+	}
+	configs.Log.Info("Connected to MongoDB!")
+	MongoDB = client.Database("dpi")
+}
